c5dd3cdbdb99e6c20870c5c95347f1da858a0527
garen <longhui.xie@nortekcontrol.com>
2021-10-27 10:24:18 +0800

    1. 支持 FFmpeg lib 移植. 并完成验证.

--- CMakeProject1/CMakeProject1.cpp
+++ CMakeProject1/CMakeProject1.cpp
@@ -1,12 +1,18 @@
 ﻿// CMakeProject1.cpp : Defines the entry point for the application.
 //
+#define __STDC_CONSTANT_MACROS
 
 #include "CMakeProject1.h"
+extern "C"
+{
+#include "libavcodec/avcodec.h "
+}
 
 using namespace std;
 
 int main()
 {
+	printf("%s", avcodec_configuration());
 	cout << "Hello CMake." << endl;
 	return 0;
 }
